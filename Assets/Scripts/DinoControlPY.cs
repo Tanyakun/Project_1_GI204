7982d1feadb64e80afa1568d3644a1404d83c71b
Tanyakun <Tanyakun.klad@bumail.net>
2025-03-24 12:00:32 +0700

    Add
    -UI Menu
    -Make Folder

---
+++ Assets/Scripts/DinoControlPY.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DinoControl : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;  // ความเร็ว (กำหนดจาก Inspector)
+    [SerializeField] private float mass = 1f;   // มวล (กำหนดจาก Inspector)
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;  // ตั้งค่ามวล
+        rb.linearVelocity = transform.forward * speed;  // ให้ Object เคลื่อนที่ไปข้างหน้า
+    }
+}
